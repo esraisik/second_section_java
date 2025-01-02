57320460215abf215962d6066578801186ba3593
isikesra <isikesra01@gmail.com>
2025-01-02 18:57:05 +0300

    v3

---
+++ src/day08_stringManipulations/C07_MailControl.java
@@ -0,0 +1,42 @@
+package day08_StringManipulations;
+
+import java.util.Scanner;
+
+public class C07_MailControl {
+
+    public static void main(String[] args) {
+
+        /*
+                     SORU : kullanicidan bir mail alin
+                     - mail @ icermiyorsa “gecersiz mail”
+                      - mail @gmail.com icermiyorsa, “mail gmail olmali”
+                     - mail @gmail.com ile bitmiyorsa, “mailde yazim hatasi var”
+                     yazdirin
+
+         */
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lütfen mail adresinizi giriniz..:");
+        String mail = scanner.nextLine();
+
+        if (!mail.contains("@")) ;
+        {
+            System.out.println("Geçersiz Mail");
+        }
+        if (!mail.contains("@gmail.com")) ;
+        {
+            System.out.println("mail gmail olmali");
+        }
+        if (!mail.endsWith("@gmail.com")) ;
+        {
+            System.out.println("mailde yazım hatası var");
+
+        }
+
+
+        // Ödev: soruyu aynı şartlarda if else statements ile çözün.
+
+
+    }
+}
