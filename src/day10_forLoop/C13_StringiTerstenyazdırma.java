37f7ad1df57210d2d4b3c2eefe31a406be43754a
isikesra <isikesra01@gmail.com>
2025-01-14 22:47:17 +0300

    v3

---
+++ src/day10_forLoop/C13_StringiTerstenyazdırma.java
@@ -0,0 +1,24 @@
+package day10_forLoop;
+
+import java.util.Scanner;
+
+public class C13_StringiTerstenyazdırma {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan bir String isteyin ve String’i tersten yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen bir metin giriniz...");
+
+        String metin = scanner.nextLine();
+
+
+        for (int i = metin.length() - 1; i >= 0; i--) {
+
+            System.out.print(metin.charAt(i));
+        }
+
+
+    }
+}
