9ed25e99627d0da42d3e5aaf4b1717f209939e60
isikesra <isikesra01@gmail.com>
2025-01-21 23:34:26 +0300

    v1

---
+++ src/day12_methodOverloading_whileLoop/C04_whileLoop.java
@@ -0,0 +1,45 @@
+package day12_methodOverloading_whileLoop;
+
+import java.util.Scanner;
+
+public class C04_whileLoop {
+
+    public static void main(String[] args) {
+
+
+           /*
+
+    While loop kullanarak kullanicidan alinan sayinin rakamlar toplamini bulun.
+
+
+    */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lütfen bir sayı giriniz: ");
+        int sayi = scan.nextInt();
+        int rakamlarToplami = 0;
+
+        while (sayi > 0) {
+            rakamlarToplami += sayi % 10;
+            sayi = sayi / 10;
+        }
+
+        System.out.println("Rakamlar Toplamı = " + rakamlarToplami);
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
